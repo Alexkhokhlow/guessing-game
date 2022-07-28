4f2670c113c0357a9f843812ba16823a7112c999
Aleksey Khokhlow <lomtik25@gmail.com>
2022-07-28 19:24:38 +0300

    feat:create task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor(max,min,result) {
+        this.max = 0;
+        this.min = 0;
+        this.result = 0;
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min
+        this.max = max
+        this.result = Math.round((max+min)/2)
+    }
 
+    guess() { 
+        return this.result
     }
 
     lower() {
-
+        this.max = this.result
+        this.setRange(this.min,this.max)
     }
 
     greater() {
-
+        this.min = this.result
+        this.setRange(this.min,this.max)
     }
 }
 
